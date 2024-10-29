0ba869a3173dfb323f7bb4f0c7deab5fce82c19e
wesley <151071658+wesley-08@users.noreply.github.com>
2024-10-29 07:35:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Title from "./components/Title"
+import Card from "./components/card"
+
+export default function App(){
+ return(
+      <div>
+        <Title />
+        <Card />
+      </div>
+      )
+}
