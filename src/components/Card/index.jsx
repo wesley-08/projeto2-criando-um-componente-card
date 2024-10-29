0a52f984833d30a75c26195d896d476d258f4fdc
wesley <151071658+wesley-08@users.noreply.github.com>
2024-10-29 08:09:37 -0300

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,7 +5,6 @@ export default function card (){
                 <img src="https://i.pinimg.com/originals/ba/94/64/ba9464145eba8762f6286a3c8387c951.jpg" alt="logo" />
             </div>
             <div className="p">
-                <h1>Pôster: Star Wars (1977)</h1>
             <p>Um pôster decorativo épico do filme Star Wars,
                 com moldura de MDF e tamanho A3 Uma ótima recordação 
                 de um dos mais icônicos filmes de todos os tempos.
